37d86d645ee7ee19a71d29c8c91a8d6ea157a70b
binlinh06 <nglinh150606@gmail.com>
2026-06-09 15:41:22 +0700

    Add RegisterRequest DTO

---
+++ src/main/java/org/example/dto/request/RegisterRequest.java
@@ -0,0 +1,29 @@
+package org.example.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    @NotBlank(message = "Full name is required")
+    private String fullName;
+
+    @Pattern(regexp = "^[0-9]{10,11}$", message = "Phone number must be 10-11 digits")
+    private String phoneNumber;
+}
